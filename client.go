9eab7006729445680c751de821373c29779ca00d
mcorbin <corbin.math@gmail.com>
2017-02-01 21:42:32 +0100

    second cleanup

--- client.go
+++ client.go
@@ -1,40 +1,40 @@
-// A Riemann client for Go, featuring concurrency, sending events and state updates, queries,
-// and feature parity with the reference implementation written in Ruby.
+// A Riemann client for Go, featuring concurrency, sending events and state updates, queries
 //
 // Copyright (C) 2014 by Christopher Gilbert <christopher.john.gilbert@gmail.com>
 package riemanngo
 
 import (
-	pb "github.com/golang/protobuf/proto"
 	"github.com/riemann/riemann-go-client/proto"
 )
 
+// Client is an interface to a generic client
+type Client interface {
+	Send(message *proto.Msg) (*proto.Msg, error)
+	Connect() error
+	Close() error
+}
+
+// request encapsulates a request to send to the Riemann server
+type request struct {
+	message     *proto.Msg
+	response_ch chan response
+}
+
+// response encapsulates a response from the Riemann server
+type response struct {
+	message *proto.Msg
+	err     error
+}
+
 // Send an event
-func SendEvent(c Client, e *Event) error {
+func SendEvent(c Client, e *Event) (*proto.Msg, error) {
 	epb, err := EventToProtocolBuffer(e)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	message := &proto.Msg{}
 	message.Events = append(message.Events, epb)
 
-	_, err = c.SendMaybeRecv(message)
-	return err
+	msg, err := c.Send(message)
+	return msg, err
 }
-
-// Query the server for events
-func QueryIndex(c TcpClient, q string) ([]Event, error) {
-	query := &proto.Query{}
-	query.String_ = pb.String(q)
-
-	message := &proto.Msg{}
-	message.Query = query
-
-	response, err := c.SendRecv(message)
-	if err != nil {
-		return nil, err
-	}
-
-	return ProtocolBuffersToEvents(response.GetEvents()), nil
-}
-
