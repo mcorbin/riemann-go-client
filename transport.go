a508c12160022f50ee38297b8acba9a408e12d46
mcorbin <corbin.math@gmail.com>
2017-02-01 20:39:49 +0100

    first cleanup

--- transport.go
+++ transport.go
@@ -7,25 +7,30 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"time"
 
 	pb "github.com/golang/protobuf/proto"
 	"github.com/riemann/riemann-go-client/proto"
 )
 
-// Transport is an interface to a generic transport used by the client
-type Transport interface {
+// Client is an interface to a generic client
+type Client interface {
 	SendRecv(message *proto.Msg) (*proto.Msg, error)
+	Connect() error
+	Close() error
 	SendMaybeRecv(message *proto.Msg) (*proto.Msg, error)
 }
 
-// TcpTransport is a type that implements the Transport interface
-type TcpTransport struct {
+// TcpClient is a type that implements the Client interface
+type TcpClient struct {
+	addr         string
 	conn         net.Conn
 	requestQueue chan request
 }
 
-// UdpTransport is a type that implements the Transport interface
-type UdpTransport struct {
+// UdpClient is a type that implements the Client interface
+type UdpClient  struct {
+	addr         string
 	conn         net.Conn
 	requestQueue chan request
 }
@@ -45,41 +50,60 @@ type response struct {
 // MAX_UDP_SIZE is the maximum allowed size of a UDP packet before automatically failing the send
 const MAX_UDP_SIZE = 16384
 
-// NewTcpTransport - Factory
-func NewTcpTransport(conn net.Conn) *TcpTransport {
-	t := &TcpTransport{
-		conn:         conn,
+// NewTcpClient - Factory
+func NewTcpClient(addr string) *TcpClient {
+	t := &TcpClient{
+		addr: addr,
 		requestQueue: make(chan request),
 	}
 	go t.runRequestQueue()
 	return t
 }
 
-// NewUdpTransport - Factory
-func NewUdpTransport(conn net.Conn) *UdpTransport {
-	t := &UdpTransport{
-		conn:         conn,
+// NewUdpClient - Factory
+func NewUdpClient(addr string) *UdpClient {
+	t := &UdpClient{
+		addr: addr,
 		requestQueue: make(chan request),
 	}
 	go t.runRequestQueue()
 	return t
 }
 
-// TcpTransport implementation of SendRecv, queues a request to send a message to the server
-func (t *TcpTransport) SendRecv(message *proto.Msg) (*proto.Msg, error) {
+func (c *TcpClient) Connect() error {
+	tcp, err := net.DialTimeout("tcp", c.addr, time.Second*5)
+	if err != nil {
+		return err
+	}
+	c.conn = tcp
+	return nil
+}
+
+
+func (c *UdpClient) Connect() error {
+	udp, err := net.DialTimeout("udp", c.addr, time.Second*5)
+	if err != nil {
+		return err
+	}
+	c.conn = udp
+	return nil
+}
+
+// TcpClient implementation of SendRecv, queues a request to send a message to the server
+func (t *TcpClient) SendRecv(message *proto.Msg) (*proto.Msg, error) {
 	response_ch := make(chan response)
 	t.requestQueue <- request{message, response_ch}
 	r := <-response_ch
 	return r.message, r.err
 }
 
-// TcpTransport implementation of SendMaybeRecv, queues a request to send a message to the server
-func (t *TcpTransport) SendMaybeRecv(message *proto.Msg) (*proto.Msg, error) {
+// TcpClient implementation of SendMaybeRecv, queues a request to send a message to the server
+func (t *TcpClient) SendMaybeRecv(message *proto.Msg) (*proto.Msg, error) {
 	return t.SendRecv(message)
 }
 
-// Close will close the TcpTransport
-func (t *TcpTransport) Close() error {
+// Close will close the TcpClient
+func (t *TcpClient) Close() error {
 	close(t.requestQueue)
 	err := t.conn.Close()
 	if err != nil {
@@ -88,8 +112,8 @@ func (t *TcpTransport) Close() error {
 	return nil
 }
 
-// runRequestQueue services the TcpTransport request queue
-func (t *TcpTransport) runRequestQueue() {
+// runRequestQueue services the TcpClient request queue
+func (t *TcpClient) runRequestQueue() {
 	for req := range t.requestQueue {
 		message := req.message
 		response_ch := req.response_ch
@@ -101,7 +125,7 @@ func (t *TcpTransport) runRequestQueue() {
 }
 
 // execRequest will send a TCP message to Riemann
-func (t *TcpTransport) execRequest(message *proto.Msg) (*proto.Msg, error) {
+func (t *TcpClient) execRequest(message *proto.Msg) (*proto.Msg, error) {
 	msg := &proto.Msg{}
 	data, err := pb.Marshal(message)
 	if err != nil {
@@ -134,21 +158,21 @@ func (t *TcpTransport) execRequest(message *proto.Msg) (*proto.Msg, error) {
 	return msg, nil
 }
 
-// UdpTransport implementation of SendRecv, will automatically fail if called
-func (t *UdpTransport) SendRecv(message *proto.Msg) (*proto.Msg, error) {
+// UdpClient implementation of SendRecv, will automatically fail if called
+func (t *UdpClient) SendRecv(message *proto.Msg) (*proto.Msg, error) {
 	return nil, fmt.Errorf("udp doesn't support receiving acknowledgements")
 }
 
-// UdpTransport implementation of SendMaybeRecv, queues a request to send a message to the server
-func (t *UdpTransport) SendMaybeRecv(message *proto.Msg) (*proto.Msg, error) {
+// UdpClient implementation of SendMaybeRecv, queues a request to send a message to the server
+func (t *UdpClient) SendMaybeRecv(message *proto.Msg) (*proto.Msg, error) {
 	response_ch := make(chan response)
 	t.requestQueue <- request{message, response_ch}
 	r := <-response_ch
 	return r.message, r.err
 }
 
-// Close will close the UdpTransport
-func (t *UdpTransport) Close() error {
+// Close will close the UdpClient
+func (t *UdpClient) Close() error {
 	close(t.requestQueue)
 	err := t.conn.Close()
 	if err != nil {
@@ -157,8 +181,8 @@ func (t *UdpTransport) Close() error {
 	return nil
 }
 
-// runRequestQueue services the UdpTransport request queue
-func (t *UdpTransport) runRequestQueue() {
+// runRequestQueue services the UdpClient request queue
+func (t *UdpClient) runRequestQueue() {
 	for req := range t.requestQueue {
 		message := req.message
 		response_ch := req.response_ch
@@ -170,7 +194,7 @@ func (t *UdpTransport) runRequestQueue() {
 }
 
 // execRequest will send a UDP message to Riemann
-func (t *UdpTransport) execRequest(message *proto.Msg) (*proto.Msg, error) {
+func (t *UdpClient) execRequest(message *proto.Msg) (*proto.Msg, error) {
 	data, err := pb.Marshal(message)
 	if err != nil {
 		return nil, err
